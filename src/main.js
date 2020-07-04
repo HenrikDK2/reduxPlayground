2f92dfd5be2aa1c31576f23865fdc74408baac1c
HenrikDK2 <Henrik265@outlook.dk>
2020-07-04 02:41:55 +0200

    Filter by input value

--- src/main.js
+++ src/main.js
@@ -5,28 +5,40 @@ import { updateUsers, filterUsers } from "./actions/index";
 import rootReducer from "./reducers";
 import "regenerator-runtime/runtime";
 
-//DOM
-const filterList = document.getElementById("filter");
-const allList = document.getElementById("all");
-
-//Store
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-store.subscribe(() => store.getState());
-
-//Update State
 (async () => {
+  //DOM
+  const filterList = document.getElementById("filter");
+  const allList = document.getElementById("all");
+  const input = document.getElementsByTagName("input")[0];
+
+  //Store
+  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+  store.subscribe(() => store.getState());
+
+  //Initial Update State
   await store.dispatch(updateUsers());
   store.dispatch(filterUsers("lea"));
   updateList();
+
+  //Events
+  input.addEventListener("keydown", () => {
+    setTimeout(() => {
+      store.dispatch(filterUsers(input.value));
+      updateList();
+    }, 0);
+  });
+
+  //Update DOM Content
+  function updateList() {
+    filterList.innerHTML = "";
+    allList.innerHTML = "";
+
+    store.getState().filter.forEach((item) => {
+      filterList.innerHTML += `<li>${item.name}</li>`;
+    });
+
+    store.getState().users.forEach((item) => {
+      allList.innerHTML += `<li>${item.name}</li>`;
+    });
+  }
 })();
-
-//Update DOM Content
-function updateList() {
-  store.getState().filter.forEach((item) => {
-    filterList.innerHTML += `<li>${item.name}</li>`;
-  });
-
-  store.getState().users.forEach((item) => {
-    allList.innerHTML += `<li>${item.name}</li>`;
-  });
-}
